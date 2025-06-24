92b8bf20585979bbda20841f9da7880c233e7547
carlos123asd <carlos-medin@hotmail.com>
2025-06-24 23:14:36 +0200

    ADD: menu and notification

--- src/components/atoms/BtnNotification.tsx
+++ src/components/atoms/BtnNotification.tsx
@@ -2,7 +2,7 @@ import { IoMdNotificationsOutline } from "react-icons/io";
 export default function BtnNotification(){
     return <>
         <div className="btnNotification bgSecundary">
-            <IoMdNotificationsOutline color="white" size={25} />
+            <IoMdNotificationsOutline className="iconNotification" color="white" size={25} />
         </div>
     </>
 }
--- src/components/organisms/Nav.tsx
+++ src/components/organisms/Nav.tsx
@@ -1,6 +1,29 @@
+import react from "../../assets/react.svg"
+import { GoHome } from "react-icons/go";
+import { FaUserTie } from "react-icons/fa";
+import { TbBrandBooking } from "react-icons/tb";
+import { MdOutlineSpeakerNotes } from "react-icons/md";
+import { FaRegBuilding } from "react-icons/fa";
+import { IoExitOutline } from "react-icons/io5";
 export default function Nav(){
+    const menu = [
+        <GoHome className="btnNav" size={45} color="#939393"/>,
+        <FaUserTie className="btnNav" size={35} color="#939393"/>,
+        <TbBrandBooking className="btnNav" size={40} color="#939393"/>,
+        <MdOutlineSpeakerNotes className="btnNav" size={35} color="#939393"/>,
+        <FaRegBuilding className="btnNav" size={35} color="#939393"/>,
+    ]
     return <>
         <div className="nav bgSecundary">
+            <img className="navIcon" src={react} alt="" />
+            <div className="contentNavIcons">
+                {
+                    menu.map((icon) => (
+                        icon
+                    ))
+                }
+            </div>
+            <IoExitOutline className="btnNav" size={40} color="#939393" />
         </div>
     </>
 }
